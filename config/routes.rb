67d40e53d9108d5cd6f001ad4855e19ae3020171
Lucas Oliveira <lcastro.oliveira@gmail.com>
2017-05-28 21:28:29 -0300

    Add home controller and index page

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
     get 'interest_points' => 'interest_points#interest_points'
     get 'beach_walkway' => 'routes#beach_walkway'
   end
+
+  #-----------------------------------------------------------------------------
+  # Map page and route route
+  #-----------------------------------------------------------------------------
+  root 'home#index'
 end
